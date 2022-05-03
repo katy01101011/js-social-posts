6e5072150069e0cd0e986b35c47de13b04f4eb98
katy01101011 <katy.91560@gmail.com>
2022-05-03 16:58:23 +0200

    add: milestone1 : objects array

--- js/script.js
+++ js/script.js
@@ -2,16 +2,60 @@ const idArray = [1];
 let newId;
 
 const posts = [
+    // {
+    //     id: 1,
+    //     "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    //     "media": "https://unsplash.it/600/300?image=171",
+    //     "author": {
+    //         "name": "Phil Mangione",
+    //         "image": "https://unsplash.it/300/300?image=15"
+    //     },
+    //     likes: 80,
+    //     "created": "2021-06-25"
+    // },
     {
         id: 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+        content: 'Hanno rubato un tir pieno di lampadine. La polizia brancola nel buio.',
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: 'Commisario Rex',
+            image: "https://unsplash.it/300/300?image=15"
         },
-        likes: 80,
-        "created": "2021-06-25"
+        likes: 67,
+        created: '2021-05-13'
+    },
+    {
+        id: 2,
+        content: 'Due cose sono infinite: l’universo e la stupidità umana, ma non sono sicuro dell’universo.',
+        media: "https://unsplash.it/600/300?image=151",
+        author: {
+            name: 'Albert Einstein',
+            image: "https://unsplash.it/300/300?image=14"
+        },
+        likes: 2,
+        created: '2022-01-03'
+    },
+    {
+        id: 3,
+        content: 'La banana alla papaya: “Conosci un buon avocado?”',
+        media: "https://unsplash.it/600/300?image=153",
+        author: {
+            name: 'Kevin Minion',
+            image: "https://unsplash.it/300/300?image=111"
+        },
+        likes: 12,
+        created: '2021-04-08'
+    },
+    {
+        id: 4,
+        content: 'Soltanto l’inutilità del primo diluvio trattiene Dio dal mandarne un secondo.',
+        media: "https://unsplash.it/600/300?image=371",
+        author: {
+            name: 'Sébastien-Roch Nicolas de Chamfort',
+            image: "https://unsplash.it/300/300?image=55"
+        },
+        likes: 56,
+        created: '2022-01-20'
     },
 ];
 console.log(posts);
